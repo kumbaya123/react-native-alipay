771a3ea983891bd38b78ac3ac1e171f820856288
Stephen <jinyaoye23@126.com>
2019-09-27 11:25:13 +0800

    更新代码

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 import { NativeModules } from 'react-native';
+// import { resolve } from 'dns';
 
-const AlipayModule = NativeModules.AlipayModule;
+const AlipayModul = NativeModules.AlipayModule;
 
 export default class AlipayModule {
-  async aliPayAction(payStr) {
+  static aliPayAction(payStr) {
     //payStr为从后台获取的支付字符串
-    Alipay.pay(payStr).then((data) => {
-      let resultDic = {};
-      /*笔者iOS端和安卓端返回的支付回调结果数据不一致，可能和支付宝sdk版本有关，
-      读者可自行根据返回数据进行相关处理，iOS(RCTAlipay.m)和安卓(AlipayModule)
-      可自行选择需要resolve回调判断处理的数据，如只返回resultStatus*/
-      if (Platform.OS === 'ios') {
-        resultDic = data[0];
-      } else {
-        resultDic = data;
-      }
-      if (resultDic.resultStatus == '9000') {
-        //支付成功
-      } else {
-        //支付失败
-      }
-    }).catch((err) => {
-      console.log('err=' + err);
-      this.refs.toast.show('支付失败');
-    });
+    return new Promise((resolve, reject) => {
+      AlipayModul.pay(payStr).then((data) => {
+        let resultDic = {};
+        /*笔者iOS端和安卓端返回的支付回调结果数据不一致，可能和支付宝sdk版本有关，
+        读者可自行根据返回数据进行相关处理，iOS(RCTAlipay.m)和安卓(AlipayModule)
+        可自行选择需要resolve回调判断处理的数据，如只返回resultStatus*/
+        if (Platform.OS === 'ios') {
+          resultDic = data[0];
+        } else {
+          resultDic = data;
+        }
+        if (resultDic.resultStatus == '9000') {
+          //支付成功
+          console.log('resolve=');
+          resolve(resultDic)
+        } else {
+          //支付失败
+          console.log('reject=');
+          reject(resultDic)
+        }
+      }).catch((err) => {
+        console.log('err=' + err);
+        this.refs.toast.show('支付失败');
+        reject(resultDic)
+      });
+    })
+
   }
 };
--- react-native-alipay.podspec
+++ react-native-alipay.podspec
@@ -14,12 +14,15 @@ Pod::Spec.new do |s|
   # s.license    = { :type => "MIT", :file => "FILE_LICENSE" }
   s.authors      = { "Your Name" => "jinyaoye23@126.com" }
   s.platforms    = { :ios => "9.0", :tvos => "10.0" }
-  s.source       = { :git => "hhttps://github.com/jinyaoye23/react-native-alipay.git", :tag => "#{s.version}" }
+  s.source       = { :git => "https://github.com/jinyaoye23/react-native-alipay.git", :tag => "#{s.version}" }
 
   s.source_files = "ios/**/*.{h,m,swift}"
   s.requires_arc = true
+  s.frameworks = "SystemConfiguration", "CoreTelephony", "QuartzCore", "CoreText", "CoreGraphics", "UIKit", "Foundation", "CFNetwork", "CoreMotion"
+  s.library = "c++", "z"
 
   s.dependency "React"
+  s.dependency 'AlipaySDK-iOS'
 	
   # s.dependency "..."
 end
