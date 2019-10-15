bd9d7c96048bae4970b952b9e1e21d6a478937f8
Stephen <jinyaoye23@126.com>
2019-10-15 09:19:24 +0800

    优化返回结果的处理

--- index.js
+++ index.js
@@ -21,25 +21,61 @@ export default class AlipayModule {
           reject(resultDic)
         } else {
           let dataJson = JSON.parse(data)
-          resultDic.resultStatus = dataJson.resultStatus
-          resultDic.content = dataJson.result ? JSON.stringify(dataJson.result) : dataJson.memo
-          resolve(resultDic)
+          let resultStatus = dataJson.resultStatus
+          let content = ''
+
+          if (resultStatus === '9000') {
+            content = JSON.stringify(dataJson.result)
+          } else if (resultStatus === '8000' || resultStatus === '6004') {
+            content = '支付处理中，支付结果以支付宝是否扣费成功为准';
+          } else if (resultStatus === '4000') {
+            content = '订单支付失败';
+          } else if (resultStatus === '5000') {
+            content = '重复请求';
+          } else if (resultStatus === '6001') {
+            content = '用户取消';
+          } else if (resultStatus === '6002') {
+            content = '网络连接出错';
+          } else {
+            content = '其它支付错误';
+          }
+          resultDic.content = content
+
+          resolve({ resultStatus, content, data: resultDic })
         }
         // resolve(resultDic)
       }).catch((err) => {
         // console.log('err-index = ', err)
         reject(err)
       });
-      // iOS 监听支付返回结果
-      emitter.once('Alipay.Resp', resp => {
-        let resultDic = {
-          resultStatus: 0,
-          content: null,
-        };
-        resultDic.resultStatus = resp.resultStatus
-        resultDic.content = resp.result ? resp.result : resp.memo
-        resolve(resultDic)
-      });
+      if (Platform.OS === 'ios')
+        // iOS 监听支付返回结果
+        emitter.once('Alipay.Resp', resp => {
+          let resultDic = {
+            resultStatus: 0,
+            content: null,
+          };
+          let resultStatus = resp.resultStatus
+          let content = ''
+          if (resultStatus === '9000') {
+            content = resp.resul
+          } else if (resultStatus === '8000' || resultStatus === '6004') {
+            content = '支付处理中，支付结果以支付宝是否扣费成功为准';
+          } else if (resultStatus === '4000') {
+            content = '订单支付失败';
+          } else if (resultStatus === '5000') {
+            content = '重复请求';
+          } else if (resultStatus === '6001') {
+            content = '用户取消';
+          } else if (resultStatus === '6002') {
+            content = '网络连接出错';
+          } else {
+            content = '其它支付错误';
+          }
+          resultDic.resultStatus = resp.resultStatus
+          resultDic.content = content
+          resolve({ resultStatus, content, data: resultDic })
+        });
     })
   }
 };
